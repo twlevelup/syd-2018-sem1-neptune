3e93abafabbd8f279367c8c18c617aed5d3c3e2b
katrina <klee@thoughtworks.com>
2018-04-03 21:13:25 +1000

    katrina/sam-updated homepage to display time in 12 hour format and added hardcoded quote

--- client/src/js/pages/homePage.js
+++ client/src/js/pages/homePage.js
@@ -2,16 +2,13 @@ require('../../styles/pages/home.scss');
 
 const BasePage = require('watch-framework').BasePage;
 const logo = require('../../images/logo.png')
-const date = new Date(Date.now()).toLocaleString().split(",")[0];
-const time = new Date(Date.now()).toLocaleString().split(",")[1];
+const time = new Date(Date.now()).toLocaleString('en-AEST', { hour12: true }).split(",")[1];
 const compiledTemplate = require("../../templates/homePage.hbs");
 
 class HomePage extends BasePage {
   template() {
     const context = {
-      date: date,
       time: time,
-      logo: logo,
     };
     return compiledTemplate(context);
   }
